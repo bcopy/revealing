34457522a9c3a7b52f50c6274e159882f80d170e
Brice Copy <brice.copy@cern.ch>
2020-06-04 17:12:38 +0200

    Initial commit

---
+++ src/main/java/com/github/bcopy/revealing/RevealingApplication.java
@@ -0,0 +1,13 @@
+package com.github.bcopy.revealing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RevealingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RevealingApplication.class, args);
+	}
+
+}
