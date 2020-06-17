246d4ed7f4430085eaa51889c7e7d6faabb59a9b
bcopy <500789+bcopy@users.noreply.github.com>
2020-06-17 19:54:04 +0200

    Cleanup imports and unused code

--- modules/cli/src/main/java/com/github/bcopy/revealing/process/ProcessorAutoConfiguration.java
+++ modules/cli/src/main/java/com/github/bcopy/revealing/process/ProcessorAutoConfiguration.java
@@ -1,22 +1,13 @@
 package com.github.bcopy.revealing.process;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.ApplicationContextAware;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @ComponentScan
-public class ProcessorAutoConfiguration implements ApplicationContextAware {
-
-   ApplicationContext applicationContext;
-   
-   @Override
-	public void setApplicationContext(ApplicationContext applicationContext) {
-		this.applicationContext = applicationContext;
-	}
+public class ProcessorAutoConfiguration {
    
    @Bean
    @ConfigurationProperties("revealing.process")
